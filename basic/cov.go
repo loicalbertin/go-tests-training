23da66e0714c6d9db9e2dffd6f8a427ed6145d81
Albertin Loic <loic.albertin@atos.net>
2018-05-15 15:27:10 +0200

    Merge branch 'master' into tests

--- basic/cov.go
+++ basic/cov.go
@@ -1,6 +1,10 @@
 package basic
 
-import "log"
+import (
+	"log"
+
+	"github.com/pkg/errors"
+)
 
 // SomeEnum is an enumeration example
 type SomeEnum int
@@ -16,16 +20,22 @@ const (
 	E4
 )
 
-// SeeCoverage is a dummy function to used to illustrate code coverage
-func SeeCoverage(e SomeEnum) {
+// EnumAsString is a dummy function to used to illustrate code coverage
+func EnumAsString(e SomeEnum) (string, error) {
 	switch e {
 	case E1:
 		log.Println("E1 given")
+		return "E1", nil
 	case E2:
 		log.Println("E2 given")
+		return "E2", nil
 	case E3:
 		log.Println("E3 given")
+		return "E3", nil
 	case E4:
 		log.Println("E4 given")
+		return "E4", nil
 	}
+
+	return "", errors.Errorf("unexpected value %d for SomeEnum", e)
 }
--- basic/cov_test.go
+++ basic/cov_test.go
@@ -1,22 +1,33 @@
 package basic
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestSeeCoverage(t *testing.T) {
+func TestEnumAsString(t *testing.T) {
 	type args struct {
 		e SomeEnum
 	}
 	tests := []struct {
-		name string
-		args args
+		name    string
+		args    args
+		want    string
+		wantErr bool
 	}{
-		{"TestE1", args{E1}},
-		{"TestE3", args{E3}},
-		{"TestE4", args{E4}},
+		{"TestE1", args{E1}, "E1", false},
+		{"TestE3", args{E3}, "E3", false},
+		{"TestE4", args{E4}, "E4", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			SeeCoverage(tt.args.e)
+			got, err := EnumAsString(tt.args.e)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EnumAsString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("EnumAsString() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }

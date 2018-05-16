45c7fcc42de2a5ca85f8a3463f6c07f8357e7b39
Albertin Loic <loic.albertin@atos.net>
2018-05-16 11:06:31 +0200

    Allow to close the http server

--- httpserver/server.go
+++ httpserver/server.go
@@ -1,13 +1,28 @@
 package httpserver
 
 import (
+	"context"
 	"net/http"
 	"net/url"
+
+	"github.com/pkg/errors"
 )
 
-func runServer(extServiceURL *url.URL) error {
+func runServer(ctx context.Context, extServiceURL *url.URL) error {
+	ch := ctx.Done()
+	if ch == nil {
+		return errors.Errorf("A cancelable context is required")
+	}
+
 	s := &server{extSrvURL: extServiceURL}
 	http.HandleFunc("/ext", s.ServeHTTPToExternal)
 	http.HandleFunc("/simple", serveSimpleHTTP)
-	return http.ListenAndServe(":12345", nil)
+
+	hs := &http.Server{Addr: ":12345"}
+	go hs.ListenAndServe()
+	go func() {
+		<-ctx.Done()
+		hs.Close()
+	}()
+	return nil
 }
